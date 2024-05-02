94be731d49462902e75fd5e07bf050ac9c09267e
vivekweb2013 <vivekweb2013@gmail.com>
2024-05-02 23:47:24 +0530

    Fix org response mapper

--- internal/domains/organization/handler.go
+++ internal/domains/organization/handler.go
@@ -87,10 +87,11 @@ func (h *handler) DeleteOrganization(w http.ResponseWriter, r *http.Request) {
 
 func (h *handler) toResponse(org Organization) *Response {
 	return &Response{
-		ID:          org.ID,
-		Name:        org.Name,
-		SuspendedAt: org.SuspendedAt,
-		Timestamps:  org.Timestamps,
+		ID:            org.ID,
+		Name:          org.Name,
+		SuspendedAt:   org.SuspendedAt,
+		BlacklistedAt: org.BlacklistedAt,
+		Timestamps:    org.Timestamps,
 	}
 }
 
