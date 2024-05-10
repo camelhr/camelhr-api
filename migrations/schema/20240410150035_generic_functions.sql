64e0a5d3e13b72bc3b1134c13d55f8867b6df737
vivekweb2013 <vivekweb2013@gmail.com>
2024-05-10 11:15:54 +0530

    Create users table

--- migrations/schema/20240410150035_generic_functions.sql
+++ migrations/schema/20240410150035_generic_functions.sql
@@ -1,10 +1,18 @@
 -- +goose Up
 -- +goose StatementBegin
--- create a generic function that prevents an operation
+
+-- create a function that prevents an operation
 CREATE OR REPLACE FUNCTION operation_not_allowed()
 RETURNS TRIGGER AS $$
 BEGIN
   RAISE EXCEPTION '% operation on table % is not allowed: %', TG_OP, TG_TABLE_NAME, TG_NAME::TEXT;
+END;
+$$ LANGUAGE plpgsql;
+
+-- create a function that generates a random token
+CREATE OR REPLACE FUNCTION random_token() RETURNS text AS $$
+BEGIN
+    RETURN md5(random()::text || clock_timestamp()::text);
 END;
 $$ LANGUAGE plpgsql;
 -- +goose StatementEnd
---
+++ migrations/schema/20240506175737_add_users_table.sql
@@ -0,0 +1,40 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    organization_id INTEGER NOT NULL,
+    email VARCHAR(255) NOT NULL CHECK (email ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'),
+    password_hash TEXT,
+    api_token text UNIQUE,
+    is_owner BOOLEAN NOT NULL DEFAULT FALSE,
+    disabled_at TIMESTAMP WITHOUT TIME ZONE,
+    comment VARCHAR(255),
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC'),
+    updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC'),
+    deleted_at TIMESTAMP WITHOUT TIME ZONE,
+    FOREIGN KEY (organization_id) REFERENCES organizations(organization_id)
+);
+
+-- create unique constraint on organization_id and email
+ALTER TABLE users ADD CONSTRAINT unique_organization_id_email UNIQUE (organization_id, email);
+
+-- create indexes
+CREATE INDEX idx_users_organization_id ON users(organization_id);
+CREATE INDEX idx_users_deleted_at ON users(deleted_at);
+
+-- create triggers to forbid truncate and delete operations on the users table
+CREATE TRIGGER prevent_truncate_on_users
+BEFORE TRUNCATE ON users
+FOR EACH STATEMENT
+EXECUTE FUNCTION operation_not_allowed();
+
+CREATE TRIGGER prevent_delete_on_users
+BEFORE DELETE ON users
+FOR EACH ROW
+EXECUTE FUNCTION operation_not_allowed();
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS users;
+-- +goose StatementEnd
